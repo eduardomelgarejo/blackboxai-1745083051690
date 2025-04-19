86dc9a5f3c74b0363eb22f0b95647b79efb4d2d9
eduardomelgarejo <eduardo.melgarejo@virginiogomez.cl>
2025-04-19 13:55:03 -0400

    Add config.py 1745085295767

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     # MySQL connection URI
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:password@localhost/box_rental')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:password@localhost/box')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
